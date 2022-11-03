2b76388b1243f6ddcb1a88cff80331f942ebb4f2
Ellednera <raphael.jongjj@gmail.com>
2022-11-03 13:43:14 +0800

    Technical changes

--- SmallSmallWorld/SinglyLinkedList.h
+++ SmallSmallWorld/SinglyLinkedList.h
@@ -13,8 +13,8 @@ typedef struct {
 } CropNode;
 
 typedef struct {
-	Crop* head;
-	Crop* tail;
+	CropNode* head;
+	CropNode* tail;
 	int total_crops;
 	int middle = MAX_CROPS / 2; // for optimization
 
@@ -23,17 +23,24 @@ typedef struct {
 // helper functions
 bool is_empty(CropsListSingle list);
 bool is_full(CropsListSingle list);
-Crop* get_head_crop(CropsListSingle list);
-Crop* get_tail_crop(CropsListSingle list);
+Crop* get_head_crop(CropsListSingle list); // returning an actual Crop object
+Crop* get_tail_crop(CropsListSingle list); // returning an actual Crop object
 
 // no modification of crop data, makes no sense :)
 void plant_crop_as_head(unsigned int crop_id);
 void plant_crop_as_tail(unsigned int crop_id);
 void plant_crop_at(unsigned int crop_id, int location);
 
+// for list manipulation, to make life easier
+CropNode* find_crop_node(unsigned int crop_id);
+void plant_crop_node_at(unsigned int crop_id, int location);
+
+// for reading data only
 Crop* find_crop(unsigned int crop_id);
 Crop* find_crop_increased_chance(unsigned int crop_id); // last to do
 
 // plants at the right side of the located crop
-void replant_crop(unsigned int crop_id, int new_location); // find_crop() + plant_crop_at()
+void replant_crop(unsigned int crop_id, int new_location); // find_crop_node() + plant_crop_node_at()
+
 void purge_crop(unsigned int crop_id);
+void purge_farm(void);
