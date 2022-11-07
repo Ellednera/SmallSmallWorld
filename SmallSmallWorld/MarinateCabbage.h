43b4c4f68c66bb6f07bd69fb7d74029595d50ca6
Ellednera <raphael.jongjj@gmail.com>
2022-11-07 15:45:37 +0800

    Technical changes

--- SmallSmallWorld/MarinateCabbage.h
+++ SmallSmallWorld/MarinateCabbage.h
@@ -5,10 +5,9 @@ typedef crops_singly_link_list MarinatingUrn;
 
 MarinatingUrn* create_stack(void);
 
-
+void observe_urn(MarinatingUrn* urn);
 
 void add_cabbage(MarinatingUrn** urn, CropNode* cabbage_node); // push
-void observe_urn(MarinatingUrn* urn);
 void remove_and_eat_cabbage(MarinatingUrn** urn); // pop
 
 void purge_urn(MarinatingUrn** urn);
